a2dabe0e5121c263a9388957827210daee41410b
crazy252 <crazy252.cg@gmail.com>
2024-08-02 13:11:14 +0200

    Add ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,16 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'TYPO3 Kate',
+    'description' => 'Increase the performance of your TYPO3 project',
+    'category' => 'misc',
+    'author' => 'crazy252',
+    'author_email' => 'crazy252.cg@gmail.com',
+    'state' => 'alpha',
+    'version' => '0.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.0.0-12.4.99',
+        ],
+    ],
+];
